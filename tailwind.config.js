b605f312d5de292b12dc4e0f20001c669fe943b9
Jacob maximus Usaraga <jlusaraga@up.edu.ph>
2022-09-28 22:56:34 +0800

    react tailwind init

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      spacing:{
+        "600": "600px",
+      }
+    },
+  },
+  plugins: [],
+}
